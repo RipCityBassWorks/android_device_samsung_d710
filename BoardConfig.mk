52af13d0a64506c7891bcde3981329a59aca0780
RipCityBassWorks <18014845+RipCityBassWorks@users.noreply.github.com>
2018-02-24 19:25:41 -0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,20 +19,20 @@
 
 -include device/samsung/galaxys2-common/BoardCommonConfig.mk
 
-TARGET_BOARD_INFO_FILE := device/samsung/d710/board-info.txt
+TARGET_BOARD_INFO_FILE := device/samsung/i9100/board-info.txt
 
 # Device specific headers
-TARGET_SPECIFIC_HEADER_PATH += device/samsung/d710/include
+TARGET_SPECIFIC_HEADER_PATH += device/samsung/i9100/include
 
 # Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/d710/bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i9100/bluetooth
 
 # Inline kernel building
 TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
-TARGET_KERNEL_CONFIG := lineageos_d710_defconfig
+TARGET_KERNEL_CONFIG := lineageos_i9100_defconfig
 
 # assert
-TARGET_OTA_ASSERT_DEVICE := galaxys2,i9100,GT-I9100,GT-I9100M,GT-I9100P,GT-I9100T,SC-02C,epic4gtouch,SPH-D710,d710,smdk4210,SPH-D710VMUB,SPH-D710BST
+TARGET_OTA_ASSERT_DEVICE := galaxys2,i9100,GT-I9100,GT-I9100M,GT-I9100P,GT-I9100T,SC-02C
 
 # Use the non-open-source parts, if they're present
--include vendor/samsung/d710/BoardConfigVendor.mk
+-include vendor/samsung/i9100/BoardConfigVendor.mk
